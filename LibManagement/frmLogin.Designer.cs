c4fc1f5f081fd13bf666f826194292594483bac7
Tosa <cornortosa007@gmail.com>
2022-05-07 14:49:38 +0700

    Add project files.

---
+++ LibManagement/frmLogin.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace LibManagement
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_register = new System.Windows.Forms.Button();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(294, 203);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(92, 27);
+            this.btn_Exit.TabIndex = 5;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(294, 131);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(100, 20);
+            this.tb_password.TabIndex = 2;
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(294, 73);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(100, 20);
+            this.tb_username.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Username";
+            // 
+            // btn_register
+            // 
+            this.btn_register.Location = new System.Drawing.Point(185, 203);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(92, 27);
+            this.btn_register.TabIndex = 4;
+            this.btn_register.Text = "Register";
+            this.btn_register.UseVisualStyleBackColor = true;
+            this.btn_register.Click += new System.EventHandler(this.btn_register_Click);
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(75, 203);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(92, 27);
+            this.btn_login.TabIndex = 3;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 321);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_register);
+            this.Controls.Add(this.btn_login);
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_register;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
+
---
+++ LibManagement/frmLogin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LibManagement
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (tb_username.Text == "" || tb_password.Text == "")
+            {
+                MessageBox.Show("Please fill in the fields");
+            }
+            else
+            {
+                SqlConnection sqlCon = new SqlConnection(@"Data Source=LAPTOP-C1548M6R\SQLEXPRESS;Initial Catalog=LibManagement;Integrated Security=True");
+                string query = "SELECT * FROM [Users] WHERE username= '" + tb_username.Text.Trim() + "' AND password='" + tb_password.Text.Trim() + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, sqlCon);
+                DataTable dtbl = new DataTable();
+                sda.Fill(dtbl);
+
+                if (dtbl.Rows.Count == 1)
+                {
+                    frmMain objfrmMain = new frmMain();
+                    this.Hide();
+                    objfrmMain.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Check your username and password");
+                }
+            }
+        }
+
+        private void btn_register_Click(object sender, EventArgs e)
+        {
+            if (tb_username.Text =="" || tb_password.Text == "")
+            {
+                MessageBox.Show("Please fill in the fields");
+            }
+            else
+            {
+                SqlConnection sqlCon = new SqlConnection(@"Data Source=LAPTOP-C1548M6R\SQLEXPRESS;Initial Catalog=LibManagement;Integrated Security=True");
+                string query = "SELECT * FROM [Users] WHERE username= '" + tb_username.Text.Trim() + "' AND password='" + tb_password.Text.Trim() + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, sqlCon);
+                DataTable dtbl = new DataTable();
+                sda.Fill(dtbl);
+                if (dtbl.Rows.Count != 1)
+                {
+                    sqlCon.Open();
+                    string queryInsert = "INSERT INTO [Users] VALUES ('" + tb_username.Text + "', '" + tb_password.Text+"' )";
+                    SqlCommand cmd = new SqlCommand(queryInsert, sqlCon);
+                    cmd.ExecuteNonQuery();
+                    sqlCon.Close();
+                    MessageBox.Show("Registereation is successfull");
+                    Clear();
+                }
+                else
+                {
+                    MessageBox.Show("The username is already Taken!");
+                }
+            }
+        }
+
+        void Clear()
+        {
+            tb_username.Text = tb_password.Text = "";
+        }
+    }
+}
